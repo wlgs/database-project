652e6d8a3081bf6668f901a685a68fe4ab1582e6
Mikołaj Wielgos <wielgosmikolaj@gmail.com>
2022-05-18 22:58:23 +0200

    implemented basic mongo connection

--- back/server.js
+++ back/server.js
@@ -1,9 +1,37 @@
+require('dotenv').config()
+
 const express = require('express');
-const mongo = require("mongodb").MongoClient
 const app = express();
+app.use(express.json())
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { exit } = require('process');
+const uri = "mongodb+srv://" + process.env.MONGO_USERNAME + ":" + process.env.MONGO_PASSWORD + "@" + process.env.MONGO_ATLAS_URL + "/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+var col_clients
+
+client.connect(err => {
+    if (err) throw err;
+    col_clients = client.db("warsaw").collection("clients");
+    // col_clients.find({}).toArray( (err, res) => {
+    //     if (err) throw err;
+    //     console.log(res);
+    // })
+    // client.close();
+});
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    col_clients.find({}).toArray( (err, resArr) => {
+        if (err) throw err;
+        var out;
+        resArr.forEach(element => {
+            out = JSON.stringify(element);
+        });
+        res.send(out);
+    })
+    
 });
 
 app.listen(4200, () => {
