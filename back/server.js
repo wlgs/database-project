e80e6049dea722e9407a9e86759c8d3f55de7e4d
Mikołaj Wielgos <wielgosmikolaj@gmail.com>
2022-05-24 18:16:46 +0200

    cors xddd

--- back/server.js
+++ back/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express');
+var cors = require('cors');
 const app = express();
 app.use(express.json())
 
@@ -16,6 +17,22 @@ var col_clients,
     col_employees,
     col_work_types,
     col_reservations
+
+var allowedOrigins = ['http://localhost:4200'];
+
+app.use(cors({
+    origin: function(origin, callback){
+      // allow requests with no origin 
+      // (like mobile apps or curl requests)
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        var msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  }));
 
 
 client.connect(err => {
