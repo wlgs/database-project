f00d5aefae5d74d044dc62959f68d05d96517c48
Mikołaj Wielgos <wielgosmikolaj@gmail.com>
2022-05-24 16:52:20 +0200

    back almost done xdddd

--- back/server.js
+++ back/server.js
@@ -9,27 +9,95 @@ const { exit } = require('process');
 const uri = "mongodb+srv://" + process.env.MONGO_USERNAME + ":" + process.env.MONGO_PASSWORD + "@" + process.env.MONGO_ATLAS_URL + "/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-var col_clients
+var col_clients,
+    col_reviews,
+    col_rooms,
+    col_rooms_types,
+    col_employees,
+    col_work_types,
+    col_reservations
+
 
 client.connect(err => {
+    console.log("Trying to connect to db...")
     if (err) throw err;
+    console.log("Retrieving collections...")
     col_clients = client.db("warsaw").collection("clients");
-    // col_clients.find({}).toArray( (err, res) => {
-    //     if (err) throw err;
-    //     console.log(res);
-    // })
-    // client.close();
+    col_reviews = client.db("warsaw").collection("reviews");
+    col_rooms_types = client.db("warsaw").collection("rooms_types");
+    col_rooms = client.db("warsaw").collection("rooms");
 });
 
 
-app.get('/', (req, res) => {
-    col_clients.find({}).toArray( (err, resArr) => {
+app.get('/clients/:id', (req, res) => {
+    console.log("Got request for -> clients/", req.params.id);
+    col_clients.findOne({_id: req.params.id}, (err, result) =>{
         if (err) throw err;
-        var out;
-        resArr.forEach(element => {
-            out = JSON.stringify(element);
-        });
-        res.send(out);
+        res.send(result);
+    })
+    
+});
+
+app.get('/clients', (req, res) => {
+    console.log("Got request for -> /clients");
+    if (req.query.skip == undefined || req.query.limit == undefined){
+        res.status(400).send("Bad params provided");
+        return
+    }
+    col_clients.find({}).sort({_idx: 1}).limit(Number(req.query.limit)).skip(Number(req.query.skip)).toArray( (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    })
+
+    
+    
+});
+
+app.get('/reviews', (req, res) => {
+    console.log("Got request for -> /reviews");
+    if (req.query.skip == undefined || req.query.limit == undefined){
+        res.status(400).send("Bad params provided");
+        return
+    }
+    col_reviews.find({}).sort({_idx: 1}).limit(Number(req.query.limit)).skip(Number(req.query.skip)).toArray( (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    })
+    
+});
+
+app.get('/reviews/:id', (req, res) => {
+    console.log("Got request for -> reviews/", req.params.id);
+    col_reviews.findOne({_id: req.params.id}, (err, result) =>{
+        if (err) throw err;
+        res.send(result);
+    })
+    
+});
+
+app.get('/rooms_types', (req, res) => {
+    console.log("Got request for -> /rooms_types");
+    col_rooms_types.find({}).toArray( (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    })
+    
+});
+
+app.get('/rooms', (req, res) => {
+    console.log("Got request for -> /rooms");
+    col_rooms.find({}).toArray( (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    })
+    
+});
+
+app.get('/rooms/:number', (req, res) => {
+    console.log("Got request for -> /rooms/", req.params.number);
+    col_rooms.findOne({room_number: req.params.number}, (err, result) =>{
+        if (err) throw err;
+        res.send(result);
     })
     
 });
